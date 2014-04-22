7464f036a3e6d7bb7b730e7d4c7d1a2ac0fcaf15
mtr <marian.trenkwalder@advokat.at>
2014-04-22 16:31:19 +0200

    projects added.

---
+++ UnitTest/UnitTest/UnitTest1.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestClass()> Public Class UnitTest1
+
+    <TestMethod()> Public Sub TestMethod1()
+        Dim c1 As VB6_1.CClass
+
+        c1 = New VB6_1.CClass
+        Assert.AreEqual(42, c1.return42)
+    End Sub
+
+End Class
